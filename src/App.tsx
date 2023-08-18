2d77eebb070454cb37f3978b5977f8e9cdf77a74
DanyilShcheblykin <danyil.shch@gmail.com>
2023-08-18 19:30:06 +0300

    Add input block

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,38 @@
+import { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { setName } from './store/userSlice';
+import './app.scss';
 
-function App() {
+const App = () => {
+  const dispatch = useDispatch();
+  const userName = useSelector((state:any) => state.user.name);
+  const [editing, setEditing] = useState(false);
+  const [newName, setNewName] = useState(userName);
+
+  const handleSave = () => {
+    dispatch(setName(newName));
+    setEditing(false);
+  };
+
   return (
-    <div>Hello</div>
+    <div className="mainBlock">
+      {editing ? (
+        <div className='nameBlock'>
+          <input
+            type="text"
+            value={newName}
+            onChange={(e) => setNewName(e.target.value)}
+          />
+          <button className='button' onClick={handleSave}>Сохранить</button>
+        </div>
+      ) : (
+        <div>
+          <h2 className='userName'>Имя пользователя: {userName}</h2>
+          <button className='button' onClick={() => setEditing(true)}>Редактировать</button>
+        </div>
+      )}
+    </div>
   );
-}
+};
 
 export default App;
